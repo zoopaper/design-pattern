73db7e71ef60419e12f809a8e0f327e3722e1dfb
krisjin <krisibm@163.com>
2015-02-15 17:16:41 +0800

    facade pattern

---
+++ src/main/java/com/pattern/facade/HomeService.java
@@ -0,0 +1,13 @@
+package com.pattern.facade;
+
+public class HomeService {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/com/pattern/facade/PersonFacade.java
@@ -0,0 +1,13 @@
+package com.pattern.facade;
+
+public class PersonFacade {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
