04db76761f55e14fa26da125fc357db0241f80ea
krisjin <krisibm@163.com>
2015-08-24 22:08:35 +0800

    template

--- src/main/java/com/pattern/jetty/AsyncEchoServlet.java
+++ src/main/java/com/pattern/jetty/AsyncEchoServlet.java
@@ -1,17 +1,11 @@
 package com.pattern.jetty;
 
-import java.io.IOException;
-import java.util.concurrent.atomic.AtomicBoolean;
-
-import javax.servlet.AsyncContext;
-import javax.servlet.ReadListener;
-import javax.servlet.ServletException;
-import javax.servlet.ServletInputStream;
-import javax.servlet.ServletOutputStream;
-import javax.servlet.WriteListener;
+import javax.servlet.*;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * <p/>
@@ -21,11 +15,9 @@ import javax.servlet.http.HttpServletResponse;
  */
 
 
-public class AsyncEchoServlet extends HttpServlet
-{
+public class AsyncEchoServlet extends HttpServlet {
     @Override
-    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
-    {
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         AsyncContext asyncContext = request.startAsync(request, response);
         asyncContext.setTimeout(0);
         Echoer echoer = new Echoer(asyncContext);
@@ -33,36 +25,31 @@ public class AsyncEchoServlet extends HttpServlet
         response.getOutputStream().setWriteListener(echoer);
     }
 
-    private class Echoer implements ReadListener, WriteListener
-    {
+    private class Echoer implements ReadListener, WriteListener {
         private final byte[] buffer = new byte[4096];
         private final AsyncContext asyncContext;
         private final ServletInputStream input;
         private final ServletOutputStream output;
         private final AtomicBoolean complete = new AtomicBoolean(false);
 
-        private Echoer(AsyncContext asyncContext) throws IOException
-        {
+        private Echoer(AsyncContext asyncContext) throws IOException {
             this.asyncContext = asyncContext;
             this.input = asyncContext.getRequest().getInputStream();
             this.output = asyncContext.getResponse().getOutputStream();
         }
 
         @Override
-        public void onDataAvailable() throws IOException
-        {
+        public void onDataAvailable() throws IOException {
             onWritePossible();
         }
 
         @Override
-        public void onAllDataRead() throws IOException
-        {
+        public void onAllDataRead() throws IOException {
             onWritePossible();
         }
 
         @Override
-        public void onWritePossible() throws IOException
-        {
+        public void onWritePossible() throws IOException {
             // This method is called:
             //   1) after first registering a WriteListener (ready for first write)
             //   2) after first registering a ReadListener iff write is ready
@@ -70,25 +57,20 @@ public class AsyncEchoServlet extends HttpServlet
             //   4) from an input callback
 
             // We should try to read, only if we are able to write!
-            while (output.isReady() && input.isReady())
-            {
+            while (output.isReady() && input.isReady()) {
                 int read = input.read(buffer);
-                if (read<0)
-                {
-                    if (complete.compareAndSet(false,true))
+                if (read < 0) {
+                    if (complete.compareAndSet(false, true))
                         asyncContext.complete();
                     break;
-                }
-                else if (read>0)
-                {
+                } else if (read > 0) {
                     output.write(buffer, 0, read);
                 }
             }
         }
 
         @Override
-        public void onError(Throwable failure)
-        {
+        public void onError(Throwable failure) {
             failure.printStackTrace();
             asyncContext.complete();
         }
