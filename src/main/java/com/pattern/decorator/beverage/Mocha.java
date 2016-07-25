ac38404e2774a761eaa959cb2d806343012670c0
krisjin <krisibm@163.com>
2016-07-26 06:48:38 +0800

    decorator

--- src/main/java/com/pattern/decorator/beverage/Mocha.java
+++ src/main/java/com/pattern/decorator/beverage/Mocha.java
@@ -7,13 +7,20 @@ package com.pattern.decorator.beverage;
  * Time: 上午6:40
  */
 public class Mocha extends CondimentDecorator {
+
+    Beverage beverage;
+
+    public Mocha(Beverage beverage) {
+        this.beverage = beverage;
+    }
+
     @Override
     public String getDescription() {
-        return "";
+        return beverage.getDescription() + ",Mocha";
     }
 
     @Override
     public double cost() {
-        return 0;
+        return 0.3 + beverage.cost();
     }
 }
