551cf85dc8a3f80fc1985186f7411687df39030b
shijingui <shijingui@jd.com>
2016-08-02 06:37:32 +0800

    command

--- src/main/java/com/pattern/commond/Client.java
+++ src/main/java/com/pattern/commond/Client.java
@@ -12,9 +12,9 @@ public class Client {
         //创建真正执行的操作的对象
         Receiver receiver = new Receiver();
         //创建具体的命令对象
-        Command comand = new ConcreteCommand(receiver);
+        Command command = new ConcreteCommand(receiver);
         //创建负责命令调用的对象
-        Invoker invoker = new Invoker(comand);
+        Invoker invoker = new Invoker(command);
         //命令调用者执行命令
         invoker.action();
     }
