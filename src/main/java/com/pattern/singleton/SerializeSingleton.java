c893923082e78a66531510760c07b0e71a98a5f2
shijingui <shijingui@jd.com>
2016-09-20 09:37:29 +0800

    SerializeSingleton

---
+++ src/main/java/com/pattern/singleton/SerializeSingleton.java
@@ -0,0 +1,29 @@
+package com.pattern.singleton;
+
+import java.io.Serializable;
+
+/**
+ * User: shijingui
+ * Date: 2016/9/20
+ */
+public class SerializeSingleton implements Serializable {
+
+    private static final long serialVersionUID = -7960628108141408430L;
+    private static SerializeSingleton instance = new SerializeSingleton();
+
+    private SerializeSingleton() {
+    }
+
+    public static SerializeSingleton getInstance() {
+        return instance;
+    }
+
+    /**
+     * 此方法在反序列化前被执行
+     *
+     * @return
+     */
+    public Object readResolve() {
+        return instance;
+    }
+}
---
+++ src/test/java/com/pattern/SerializeSingletonTest.java
@@ -0,0 +1,28 @@
+package com.pattern;
+
+import com.pattern.singleton.SerializeSingleton;
+import org.junit.Test;
+
+import java.io.*;
+
+/**
+ * User: shijingui
+ * Date: 2016/9/20
+ */
+public class SerializeSingletonTest {
+
+    @Test
+    public void test() throws IOException, ClassNotFoundException {
+        File file = new File("Singleton.out");
+        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file));
+        SerializeSingleton serializeSingleton = SerializeSingleton.getInstance();
+        oos.writeObject(serializeSingleton);
+        oos.close();
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file));
+        ObjectInputStream ois2 = new ObjectInputStream(new FileInputStream(file));
+        Object obj1 = ois.readObject();
+        Object obj2 = ois2.readObject();
+        ois.close();
+        System.out.println(obj1 == obj2);
+    }
+}
