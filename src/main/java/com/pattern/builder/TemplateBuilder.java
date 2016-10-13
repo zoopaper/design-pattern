1725ecb423f05fd26686a5442b05238dc971e234
shijingui <shijingui@jd.com>
2016-10-13 20:55:29 +0800

    delete

--- src/main/java/com/pattern/builder/TemplateBuilder.java
+++ src/main/java/com/pattern/builder/TemplateBuilder.java
@@ -5,31 +5,13 @@ package com.pattern.builder;
  * @date 2015-2-11
  */
 public interface TemplateBuilder {
-	
-	public String templateHtml="";
 
-	/**
-	 * 构建模板的标题
-	 * 
-	 * @param ctx
-	 * @param html
-	 */
-	public TemplateBuilder title();
+    public String templateHtml = "";
 
-	/**
-	 * 构建模板摘要
-	 * 
-	 * @param ctx
-	 * @param html
-	 */
-	public TemplateBuilder summary();
+    public TemplateBuilder title();
 
-	/**
-	 * 构建模板内容
-	 * 
-	 * @param ctx
-	 * @param html
-	 */
-	public TemplateBuilder context();
+    public TemplateBuilder summary();
+
+    public TemplateBuilder context();
 
 }
