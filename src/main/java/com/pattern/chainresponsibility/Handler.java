3f6e07968ba2322cdf7495478f7522c0c96889b3
krisjin <krisibm@163.com>
2020-05-04 15:24:43 +0800

    chain

--- src/main/java/com/pattern/chainresponsibility/Handler.java
+++ src/main/java/com/pattern/chainresponsibility/Handler.java
@@ -8,5 +8,12 @@ package com.pattern.chainresponsibility;
  */
 public abstract class Handler {
 
+    protected Handler successor;
 
+    public void setSuccessor(Handler successor) {
+        this.successor = successor;
+    }
+
+
+    public abstract void handelRequest(RequestType requestType);
 }
---
+++ src/main/java/com/pattern/chainresponsibility/RequestType.java
@@ -0,0 +1,11 @@
+package com.pattern.chainresponsibility;
+
+/**
+ * User:krisjin
+ * Date:2020-05-04
+ */
+public enum RequestType {
+
+    INIT, UPLOD;
+
+}
