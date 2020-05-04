df25ae0ed888f495b266879342d1adb992713582
krisjin <krisibm@163.com>
2020-05-04 14:41:58 +0800

    chain

---
+++ src/main/java/com/pattern/chainresponsibility/Handler.java
@@ -0,0 +1,12 @@
+package com.pattern.chainresponsibility;
+
+/**
+ * 这是一个抽象类，提供给所有实际处理器进行继承。
+ * 它拥有一个handleRequest,用来接收需要处理的请求。
+ * User:krisjin
+ * Date:2020-05-04
+ */
+public abstract class Handler {
+
+
+}
