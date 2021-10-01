e481f60a87759209541582152de0468243e9f8bf
krisjin <krisibm@163.com>
2021-10-02 00:52:49 +0800

    test

--- src/main/java/com/pattern/adapter/peg/RoundPeg.java
+++ src/main/java/com/pattern/adapter/peg/RoundPeg.java
@@ -4,9 +4,6 @@ package com.pattern.adapter.peg;
  * 定义一个适配者接口IRoundPeg，具体的适配者实现，要继承它，为什么要这么设计呢
  * 适配者也完全可以定义为具体的类，当动手实践的时候，要是这么做的话，发现阅读性不好，
  * 当然还是看实际的场景需要，选择是接口定义还是具体实现类，多思考一下
- * <p>
- * User: krisjin
- * Date: 2016/9/21
  */
 public interface RoundPeg {
 
--- src/main/java/com/pattern/adapter/peg/RoundPegImpl.java
+++ src/main/java/com/pattern/adapter/peg/RoundPegImpl.java
@@ -2,8 +2,6 @@ package com.pattern.adapter.peg;
 
 /**
  * 圆形桩，在新的接口中使用该圆形状，可以创建一个圆形桩的适配器
- * User: krisjin
- * Date: 2016/9/21
  */
 public class RoundPegImpl implements RoundPeg {
     @Override
